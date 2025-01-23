b2dc0d6b541f553365ee0ce5caa13b5cb8603f66
huda-7077 <85928499+huda-7077@users.noreply.github.com>
2025-01-23 23:27:16 +0700

    chore: initial setup

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import cors from "cors";
+import express, { NextFunction, Request, Response } from "express";
+import sampleRouter from "./routes/sample.router";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+//routes
+app.use("/samples", sampleRouter);
+
+// middleware error
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(400).send(err.message);
+});
+
+export default app;
