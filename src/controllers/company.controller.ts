02aa8ebde875b01753514d597048b620bac3518a
calvin-key <calvin.delanop7@gmail.com>
2025-02-16 10:28:24 +0700

    feat: get companies

--- src/controllers/company.controller.ts
+++ src/controllers/company.controller.ts
@@ -1,6 +1,28 @@
 import { NextFunction, Request, Response } from "express";
 import { updateCompanyProfileService } from "../services/company/update-company-profile.service";
 import { getCompanyProfileService } from "../services/company/get-company-profile.service";
+import { getCompaniesService } from "../services/company/get-companies.service";
+
+export const getCompaniesController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const query = {
+      take: parseInt(req.query.take as string) || 3,
+      page: parseInt(req.query.page as string) || 1,
+      sortBy: (req.query.sortBy as string) || "createdAt",
+      sortOrder: (req.query.sortOrder as string) || "desc",
+      search: (req.query.search as string) || "",
+      location: (req.query.location as string) || "",
+    };
+    const result = await getCompaniesService(query);
+    res.status(200).send(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const getCompanyProfileController = async (
   req: Request,
