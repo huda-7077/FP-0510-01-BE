c0c20b778999e67d12918c04426988b1824aaf06
calvin-key <calvin.delanop7@gmail.com>
2025-03-05 06:02:49 +0700

    Fix spj 34 (#35)
    * fix: filtered by province on regency cached data
    
    * fix: get job detail

--- src/services/job/get-job.service.ts
+++ src/services/job/get-job.service.ts
@@ -1,12 +1,29 @@
 import { Prisma } from "@prisma/client";
 import { prisma } from "../../lib/prisma";
 
-export const getJobService = async (slug: string, companyId?: number) => {
+export const getJobService = async (
+  slug: string,
+  companyId?: number,
+  isPublished?: boolean,
+  isExpired?: boolean
+) => {
   try {
     const whereClause: Prisma.JobWhereUniqueInput = { slug, isDeleted: false };
 
     if (companyId) {
       whereClause.companyId = companyId;
+    }
+
+    if (isPublished) {
+      whereClause.isPublished = isPublished;
+    }
+
+    if (isExpired !== undefined) {
+      if (isExpired === false) {
+        whereClause.applicationDeadline = { gt: new Date() };
+      } else {
+        whereClause.applicationDeadline = { lte: new Date() };
+      }
     }
 
     const job = await prisma.job.findUnique({
